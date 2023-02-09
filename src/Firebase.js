ab5de724dcdd0e179d7a46d64335e2aec3d19a77
Khrononian <Alv.Barnes@gmail.com>
2023-02-09 18:57:21 -0500

    New installs

---
+++ src/Firebase.js
@@ -0,0 +1,60 @@
+import React from 'react'
+// import { initializeApp } from "firebase/app";
+// import { getAuth, signInWithCustomToken, createUserWithEmailAndPassword, signOut } from "firebase/auth"
+// import { initializeApp } from 'firebase-admin';
+
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./steam-clone-ba33d-firebase-adminsdk-zreqj-bfd9b4f447.json')
+
+const Firebase = () => {
+    
+
+    // const firebaseConfig = {
+    //     apiKey: "AIzaSyBCRdufWIqxKTPz_J7p1Zb05Ha9ssj5n3Y",
+    //     authDomain: "steam-clone-ba33d.firebaseapp.com",
+    //     projectId: "steam-clone-ba33d",
+    //     storageBucket: "steam-clone-ba33d.appspot.com",
+    //     messagingSenderId: "65364662879",
+    //     appId: "1:65364662879:web:6bed306f967850ed7e99ea",
+    //     measurementId: "G-JP8R3ZYNDC"
+    // };
+    // // admin.initializeApp({
+    // //     credential: admin.credential.cert(serviceAccount),
+    // //     serviceAccountId: "firebase-adminsdk-zreqj@steam-clone-ba33d.iam.gserviceaccount.com"
+    // // })
+    // // admin.initializeApp()
+    // const id = 'some-id'
+
+    // // admin.auth().createCustomToken(id)
+    // // .then((customToken) => {
+    // //     console.log('CHECKS', customToken)
+    // // })
+
+    // const app = initializeApp(firebaseConfig, 'Primary');
+    // const auth = getAuth(app);
+    
+
+    // signInWithCustomToken(auth, 'some-uid')
+    // .then((userCredential) => {
+    //     console.log('CHECKS', userCredential)
+    // })
+
+    // getAuth().createCustomToken(id)
+    // .then((customToken) => {
+    //     signInWithCustomToken(getAuth(), customToken)
+    //     .then((userCredential) => {
+    //         console.log('FINALLY', userCredential.user)
+    //     })
+    // })
+    
+    // console.log('Firebase', app)
+    console.log('Firebase')
+    return (
+        
+        <div>
+            Test
+        </div>
+    )
+}
+
+export default Firebase
