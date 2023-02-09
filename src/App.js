76747b34e39f308af162018f0c72b79daf2f70f9
Khrononian <Alv.Barnes@gmail.com>
2023-02-09 18:58:42 -0500

    Added context

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
+import React, { useEffect, useState, useContext } from 'react'
 import { Tabs, Tab } from '@mui/material'
 import Nav from './Nav'
+import { Context } from './ContextData';
 import SiteNav from './SiteNav';
 import Games from './Games';
 import Categories from './Categories';
+import Login from './Routes/Login';
 import './App.css';
 
-import React from 'react'
 
 const App = () => {
+  // const [games, setGames] = useState([])
+  // const [singleGame, setSingleGame] = useState([])
+  // const [log, setLog] = useState(false);
+
+  const newContext = useContext(Context)
+
+  // useEffect(() => {
+  //   fetch()
+  //   .then(response => response.json())
+  //   .then(data => {
+
+  //   })
+  // }, [])
+  
+  console.log('Cont', newContext)
   return (
-    <main>
-      <Nav />
-      <SiteNav />
-      <Games />
-      <Categories />
-    </main>
+    
+      <main>
+        <Nav />
+        <SiteNav />
+        <Games />
+        <Categories />
+        {/* <Login /> */}
+      </main>
     
   )
 }
