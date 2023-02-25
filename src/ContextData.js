5a08d8d9f87e41cd109f3eb143c8cd95e177b1f5
Khrononian <Alv.Barnes@gmail.com>
2023-02-24 20:10:13 -0500

    New states and exports

--- src/ContextData.js
+++ src/ContextData.js
@@ -6,12 +6,25 @@ const ContextData = ({ children }) => {
     const [games, setGames] = useState([])
     const [singleGame, setSingleGame] = useState([])
     const [featuredGames, setFeaturedGames] = useState([])
+    const [belowGames, setBelowGames] = useState([])
+    const [updatedGames, setUpdatedGames] = useState([])
     const [log, setLog] = useState(false);
+
+    const setGamePage =  (event) => {
+        // USE THIS TO SET THE GAME CLICKED ON(LINK) TO THE SINGLE GAME STATE
+        console.log('Event', event, event.target.dataset.appid)
+        if (singleGame.length > 1) setSingleGame(current => current.filter(game => game.appID === event.target.dataset.appid))
+        fetch(`https://api.steamapis.com/market/app/${ event.target.dataset.appid }?api_key=USh9nTgdKcpomNdTTl-Iok0OjDA`)
+        .then(response => response.json())
+        .then(data => setSingleGame(prev => prev.concat(data)))
+    }
 
     useEffect(() => {
         fetch('https://api.steamapis.com/market/apps?api_key=USh9nTgdKcpomNdTTl-Iok0OjDA')
         .then(response => response.json())
         .then(data => {
+            const randomGame = data.filter((item, index, array) => item.is_free === false ).filter(game => game.price_overview !== undefined ).filter(single => single.price_overview.final_formatted  ).filter(single => Number(single.price_overview.final_formatted.substring(1)) < 10 )
+            
             console.log('DATA', data, data.find(item => item.name === '7 Days to Die'))
             console.log('Context', featuredGames, data[Math.floor(Math.random() * data.length)])
             setGames(prev => prev.concat(data))
@@ -22,22 +35,32 @@ const ContextData = ({ children }) => {
                 .then(single => {
                     
                     setFeaturedGames(prev => prev.concat(single))
+                })
+            }
+            for (let i = 0; i < 4; i++) {
+                fetch(`https://api.steamapis.com/market/app/${ randomGame[Math.floor(Math.random() * 100)].appID }?api_key=USh9nTgdKcpomNdTTl-Iok0OjDA`)
+                .then(response => response.json())
+                .then(single => {
+                    setBelowGames(prev => prev.concat(single))
                     
                 })
-                setSingleGame(prev => prev.concat(
-                    data.filter(name => name === data.find(item => item['Rimworld']))
-                ))
             }
-            
-            
-            
+
+            for (let i = 0; i < 7; i++) {
+                fetch(`https://api.steamapis.com/market/app/${ data[Math.floor(Math.random() * data.length)].appID }?api_key=USh9nTgdKcpomNdTTl-Iok0OjDA`)
+                .then(response => response.json())
+                .then(single => {
+                    
+                    setUpdatedGames(prev => prev.concat(single))
+                })
+            }
         })
         .catch(error => console.log(error))
     }, [])
     
 
     return (
-        <Context.Provider value={ { games, setGames, singleGame, setSingleGame, featuredGames, log, setLog } }>
+        <Context.Provider value={ { games, setGames, singleGame, setSingleGame, featuredGames, belowGames, updatedGames, log, setLog, setGamePage } }>
             {children}
         </Context.Provider>
     )
