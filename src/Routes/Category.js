9833b89829b3d218d2709eb77efbbc980c33f1f8
Khrononian <Alv.Barnes@gmail.com>
2023-02-27 22:09:22 -0500

    Added states, useEffect and mapped data

--- src/Routes/Category.js
+++ src/Routes/Category.js
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Nav from '../Nav'
 import SiteNav from '../SiteNav'
+import { Context } from '../ContextData'
 import { WindowSharp } from '@mui/icons-material'
 import AppleIcon from '@mui/icons-material/Apple';
 import { Card, CardMedia, CardActionArea, CardContent } from '@mui/material';
 import './category.css'
+import { useLocation, Link } from 'react-router-dom'
 
 const Category = () => {
+  const [categoryGame, setCategoryGames] = useState([])
+  const [incomingGames, setIncomingGames] = useState([])
+  const categoryContext = useContext(Context)
+  const location = useLocation()
+
+  useEffect(() => {
+    for (let i = 0; i < 14; i++) {
+      fetch(`https://api.steamapis.com/market/app/${ categoryContext.games[Math.floor(Math.random() * categoryContext.games.length)].appID }?api_key=USh9nTgdKcpomNdTTl-Iok0OjDA`)
+      .then(response => response.json())
+      .then(games => {
+          
+          setCategoryGames(prev => prev.concat(games))
+      })
+    }
+    for (let i = 0; i < 3; i++) {
+      fetch(`https://api.steamapis.com/market/app/${ categoryContext.games[Math.floor(Math.random() * categoryContext.games.length)].appID }?api_key=USh9nTgdKcpomNdTTl-Iok0OjDA`)
+      .then(response => response.json())
+      .then(games => {
+          
+          setIncomingGames(prev => prev.concat(games))
+      })
+    }
+  }, [])
+
+  console.log('INSIDE PAGE', categoryContext, categoryGame, location)
 
   return (
+    categoryGame[0] !== undefined ?
+
     <div>
       <Nav />
       <SiteNav />
       <div className='main-cat'>
-        <h2>HORROR</h2>
+        <h2>{location.state !== null ? location.state.name : ''}</h2>
         <div className='top'>
           <div className='top-left'>
             <img src='https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/hero_capsule.jpg?t=1674441703' alt='Game' />
@@ -51,260 +80,132 @@ const Category = () => {
         <h2>TITLES</h2>
         <div className='titles'>
           <div className='first-title'>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
+            {categoryGame.filter((_, index) => index < 2).map(game => (
+              
+              <Card className='title-card'>
+                <Link to={'/app/referer'} onClick={categoryContext.setGamePage} data-appid={game.appID}>
+                  <CardActionArea>
+                    <CardMedia
+                      className='title-img'
+                      component='img'
+                      height='140' 
+                      image={game.header_image}
+                    />
+                    <CardContent className='bot'>
+                      <WindowSharp />
+                      <p>{game.is_free === false ? game.price_overview.final_formatted : 'Free To Play'}</p>
+                    </CardContent>
+                  </CardActionArea>  
+                </Link>
+              </Card>
+            ))}
           </div>
           <div className='second-title'>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
+            {categoryGame.filter((_, index) => index > 1 && index < 5).map(game => (
+              <Card className='title-card'>
+                <Link to={'/app/referer'} onClick={categoryContext.setGamePage} data-appid={game.appID}>
+                  <CardActionArea>
+                    <CardMedia
+                      className='title-img'
+                      component='img'
+                      height='140' 
+                      image={game.header_image}
+                    />
+                    <CardContent className='bot'>
+                      <WindowSharp />
+                      <p>{game.is_free === false ? game.price_overview.final_formatted : 'Free To Play'}</p>
+                    </CardContent>
+                  </CardActionArea>  
+                </Link>
+              </Card>
+            ))}
           </div>
           <div className='third-title'>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
+            {categoryGame.filter((_, index) => index > 4 && index < 9).map(game => (
+              <Card className='title-card'>
+                <Link to={'/app/referer'} onClick={categoryContext.setGamePage} data-appid={game.appID}>
+                  <CardActionArea>
+                    <CardMedia
+                      className='title-img'
+                      component='img'
+                      height='140' 
+                      image={game.header_image}
+                    />
+                    <CardContent className='bot'>
+                      <WindowSharp />
+                      <p>{game.is_free === false ? game.price_overview.final_formatted : 'Free To Play'}</p>
+                    </CardContent>
+                  </CardActionArea>  
+                </Link>
+              </Card>
+            ))}
           </div>
           <div className='second-title'>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
+            {categoryGame.filter((_, index) => index > 8 && index < 12).map(game => (
+              <Card className='title-card'>
+                <Link to={'/app/referer'} onClick={categoryContext.setGamePage} data-appid={game.appID}>
+                  <CardActionArea>
+                    <CardMedia
+                      className='title-img'
+                      component='img'
+                      height='140' 
+                      image={game.header_image}
+                    />
+                    <CardContent className='bot'>
+                      <WindowSharp />
+                      <p>{game.is_free === false ? game.price_overview.final_formatted : 'Free To Play'}</p>
+                    </CardContent>
+                  </CardActionArea>  
+                </Link>
+              </Card>
+            ))}
           </div>
           <div className='first-title'>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
-            <Card className='title-card'>
-              <CardActionArea>
-                <CardMedia
-                  className='title-img'
-                  component='img'
-                  height='140' 
-                  image='https://cdn.cloudflare.steamstatic.com/steam/apps/1693980/header.jpg?t=1675106922'
-                />
-                <CardContent className='bot'>
-                  <WindowSharp />
-                  <p></p>
-                </CardContent>
-              </CardActionArea>  
-            </Card>
+            {categoryGame.filter((_, index) => index > 11 && index < 14).map(game => (
+              <Card className='title-card'>
+                <Link to={'/app/referer'} onClick={categoryContext.setGamePage} data-appid={game.appID}>
+                  <CardActionArea>
+                    <CardMedia
+                      className='title-img'
+                      component='img'
+                      height='140' 
+                      image={game.header_image}
+                    />
+                    <CardContent className='bot'>
+                      <WindowSharp />
+                      <p>{game.is_free === false ? game.price_overview.final_formatted : 'Free To Play'}</p>
+                    </CardContent>
+                  </CardActionArea>  
+                </Link>
+              </Card>
+            ))}
           </div>
         </div>
         <h2>COMING SOON</h2>
         <div className='new-titles'>
-          <Card className='coming-soon'>
-            <CardActionArea>
-              <CardMedia 
-                className='soon-img'
-                component='img'
-                // height='133.97'
-                image='https://cdn.cloudflare.steamstatic.com/steam/apps/1326470/header.jpg?t=1675195445'
-              />
-              <CardContent className='bot'>
-                <WindowSharp />
-                <p>Coming Soon</p>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card className='coming-soon'>
-            <CardActionArea>
-              <CardMedia 
-                className='soon-img'
-                component='img'
-                // height='133.97'
-                image='https://cdn.cloudflare.steamstatic.com/steam/apps/1326470/header.jpg?t=1675195445'
-              />
-              <CardContent className='bot'>
-                <WindowSharp />
-                <p>Coming Soon</p>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card className='coming-soon'>
-            <CardActionArea>
-              <CardMedia 
-                className='soon-img'
-                component='img'
-                // height='133.97'
-                image='https://cdn.cloudflare.steamstatic.com/steam/apps/1326470/header.jpg?t=1675195445'
-              />
-              <CardContent className='bot'>
-                <WindowSharp />
-                <p>Coming Soon</p>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+          {incomingGames.map(game => (
+            <Card className='coming-soon'>
+              <Link to={'/app/referer'} onClick={categoryContext.setGamePage} data-appid={game.appID}>
+                <CardActionArea>
+                  <CardMedia 
+                    className='soon-img'
+                    component='img'
+                    // height='133.97'
+                    image={game.header_image}
+                  />
+                  <CardContent className='bot'>
+                    <WindowSharp />
+                    <p>Coming Soon</p>
+                  </CardContent>
+                </CardActionArea>
+              </Link>
+            </Card>
+          ))}
           
         </div>
       </div>
     </div>
+    : null
   )
 }
 
