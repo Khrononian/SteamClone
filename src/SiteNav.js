f8ad77d41a428526d4264e8931ead0c6958368ab
Khrononian <Alv.Barnes@gmail.com>
2023-04-03 22:13:42 -0400

    Removed buttongroup

--- src/SiteNav.js
+++ src/SiteNav.js
@@ -16,33 +16,6 @@ const SiteNav = () => {
             <Button>Points Shop</Button>
             <Button>News</Button>
             <Button>Labs</Button>
-            <ButtonGroup>
-                <div className='left'>
-                    <Button>Free to Play</Button>
-                    <Button>Demos</Button>
-                    <Button>Early Access</Button>
-                    <Button>Controller-friendly</Button>
-                    <Button>Remote Play</Button>
-                    <Button>Software</Button>
-                    <Button>Soundtrack</Button>
-                    <Button>VR Titles</Button>
-                    <Button>VR Hardware</Button>
-                    <Button>Steam Deck</Button>
-                    <Button>Great on Deck</Button>
-                    <Button>macOS</Button>
-                    <Button>SteamOS + Linux</Button>
-                    <Button>For PC Cafes</Button>
-                </div>
-                <div className='right'>
-                    <Button>Action</Button>
-                    <Button>Adventure</Button>
-                    <Button>Role-Playing</Button>
-                    <Button>Stimulation</Button>
-                    <Button>Strategy</Button>
-                    <Button>Sports & Racing</Button>
-
-                </div>
-            </ButtonGroup>
         </div>
         <div className='textfield'>
             <input className='search' type='text' placeholder='Search' />
